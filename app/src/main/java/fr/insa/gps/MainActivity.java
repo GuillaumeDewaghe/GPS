197623cc62832c9ec8be5c0d25389012178b269e
GuillaumeDewaghe <55316421+GuillaumeDewaghe@users.noreply.github.com>
2022-02-14 13:05:31 +0100

    Début

--- app/src/main/java/fr/insa/gps/MainActivity.java
+++ app/src/main/java/fr/insa/gps/MainActivity.java
@@ -2,13 +2,37 @@ package fr.insa.gps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonP2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.buttonP2 = findViewById(R.id.button3);
+
+        buttonP2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent otherActivity = new Intent(getApplicationContext(), Page2Activity.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
+
+        buttonP2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return false;
+            }
+        });
     }
 }
---
+++ app/src/main/java/fr/insa/gps/Page2Activity.java
@@ -0,0 +1,14 @@
+package fr.insa.gps;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Page2Activity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2);
+    }
+}
